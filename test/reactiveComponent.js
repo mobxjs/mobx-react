66ed444f0ecc696e931a64c8ad83f9532d649b8a
Jiří Špác <capajj@gmail.com>
2015-11-25 00:29:44 +0100

    stateless component props assertions added

--- test/reactiveComponent.js
+++ test/reactiveComponent.js
@@ -60,12 +60,12 @@ function shallow(component, props) {
 exports.testNestedRendering = function(test) {
     var list$ = shallow(todoList, { store: store });
     var listResult = list$.getRenderOutput();
-    
+
     test.equal(todoListRenderings, 1, "should have rendered list once");
     test.equal(listResult.props.children.length, 2, "list should have two children");
     test.equal(listResult.props.children[0].props.children, "1", "should have correct title");
     test.equal(listResult.props.children[1].length, 1, "list should on item in the list");
-    
+
     var item1$ = shallow(todoItem, listResult.props.children[1][0].props);
     item1$.getRenderOutput();
     test.equal(todoItemRenderings, 1, "item1 should render once");
@@ -77,7 +77,7 @@ exports.testNestedRendering = function(test) {
     test.equal(todoItemRenderings, 2, "item1 should have rendered twice");
     test.equal(mobservable.extras.getDNode(store, "todos").observers.length, 1, "observers count shouldn't change");
     test.equal(mobservable.extras.getDNode(store.todos[0], "title").observers.length, 1, "title observers should not have increased");
-    
+
     store.todos.push({
         title: "b",
         completed: true
@@ -85,25 +85,25 @@ exports.testNestedRendering = function(test) {
 
     listResult = list$.getRenderOutput();
     test.equal(listResult.props.children[1].length, 2, "list should two items in in the list");
-    
+
     var item2$ = shallow(todoItem, listResult.props.children[1][1].props);
     item2$.getRenderOutput();
-    
+
     test.equal(todoListRenderings, 2, "should have rendered list twice");
-    test.equal(todoItemRenderings, 3, "item2 should have rendered as well"); 
+    test.equal(todoItemRenderings, 3, "item2 should have rendered as well");
     test.equal(mobservable.extras.getDNode(store.todos[1], "title").observers.length, 1, "title observers should have increased");
     test.equal(mobservable.extras.getDNode(store.todos[1], "completed").observers.length, 0, "completed observers should not have increased");
-    
+
     var oldTodo = store.todos.pop();
     listResult = list$.getRenderOutput();
     test.equal(todoListRenderings, 3, "should have rendered list another time");
-    test.equal(todoItemRenderings, 3, "item1 should not have rerendered"); 
+    test.equal(todoItemRenderings, 3, "item1 should not have rerendered");
     test.equal(listResult.props.children[1].length, 1, "list should have only on item in list now");
-    
+
     item2$.unmount(); // that's what react does normally for us..
     test.equal(mobservable.extras.getDNode(oldTodo, "title").observers.length, 0, "title observers should have decreased");
     test.equal(mobservable.extras.getDNode(oldTodo, "completed").observers.length, 0, "completed observers should not have decreased");
-    
+
     test.done();
 }
 
@@ -112,7 +112,7 @@ exports.testIsComponentReactive = function(test) {
     test.equal(component.isMobservableReactObserver, true);
     test.equal(mobservable.isObservable(component), false); // dependencies not known yet
     test.equal(mobservable.isObservable(component.render), false); // dependencies not known yet
-    
+
     component.componentWillMount();
     component.render();
     test.equal(mobservable.isObservable(component.render), true); // dependencies not known yet
@@ -145,29 +145,63 @@ exports.testKeepViewsAlive = function(test) {
         },
         z: "hi"
     });
-    
+
     var component = observer(function testComponent() {
         return React.createElement("div", {}, data.z + data.y);
     });
-    
+
     var c$ = shallow(component, { });
 
     var cResult = c$.getRenderOutput();
     test.equal(yCalcCount, 1);
     test.equal(cResult.props.children, "hi6");
-    
+
     data.z = "hello";
     // test: rerender should not need a recomputation of data.y because the subscription is kept alive
-    
+
     test.equal(yCalcCount, 1);
 
     cResult = c$.getRenderOutput();
     test.equal(cResult.props.children, "hello6");
     test.equal(yCalcCount, 1);
-    
+
     test.equal(mobservable.extras.getDNode(data, "y").observers.length, 1);
     c$.unmount();
     test.equal(mobservable.extras.getDNode(data, "y").observers.length, 0);
 
     test.done();
+}
+
+var stateLessComp = function() {
+    todoListRenderings++;
+    var todos = this.props.store;
+    return e("div", {},
+        e("h1", null, todos.todos.length),
+        todos.todos.map(function(todo, idx) {
+            return e(todoItem, {
+                key: idx,
+                todo: todo
+            });
+        })
+    );
+}
+stateLessComp.propTypes = {
+  testProp: React.PropTypes.string
+}
+stateLessComp.defaultProps = {
+  testProp: 'default value for prop testProp'
+}
+
+exports.testStatelessComponentProps = function(test) {
+    var statelessCompObserver = observer(stateLessComp);
+    test.equal(statelessCompObserver.getDefaultProps().testProp, 'default value for prop testProp', "default property value should be propagated");
+    var originalConsoleError = console.error
+    var beenWarned = false
+    console.error = function() {
+      beenWarned = true;
+    }
+    React.createElement(statelessCompObserver, {testProp: 10})
+    console.error = originalConsoleError;
+    test.equal(beenWarned, true, "an error should be logged with a property type warning")
+    test.done()
 }
