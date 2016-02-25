d61274a3ad69a943aca56a1c5d174dfc39019de0
Michel Weststrate <mweststrate@gmail.com>
2016-02-25 17:18:04 +0100

    Fixed some merge issues

--- test/ts/compile-ts.tsx
+++ test/ts/compile-ts.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Component} from 'react'; 
-import {} from 'mobservable';
 import {observer} from '../../';
 
 @observer
