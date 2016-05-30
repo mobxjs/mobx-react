4fc3066136a843d8cc473a82558e7fe9425a48ac
phestermcs <phestermcs@gmail.com>
2016-05-30 09:51:08 -0600

    fix - set decorator return type to void

--- index.d.ts
+++ index.d.ts
@@ -9,6 +9,6 @@ export function observer<P>(clazz: React.ClassicComponentClass<P>): React.Classi
 export function observer<P>(clazz: React.ComponentClass<P>): React.ComponentClass<P>;
 export function observer<TFunction extends React.ComponentClass<any>>(target: TFunction): TFunction; // decorator signature
 
-export function property(propType: React.Validator<any>, defaultValue?: any)
-export function state(propType: React.Validator<any>, defaultValue?: any)
-export function context(propType: React.Validator<any>, defaultValue?: any, nameInContext?: string)
+export function property(propType: React.Validator<any>, defaultValue?: any): void
+export function state(propType: React.Validator<any>, defaultValue?: any): void
+export function context(propType: React.Validator<any>, defaultValue?: any, nameInContext?: string): void
