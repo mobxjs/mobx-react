1e5d1a5a5a3b57cb878f2cfe02a2ebef523cc293
Michel Weststrate <mweststrate@gmail.com>
2016-11-07 13:54:07 +0100

    added tests

--- test/observer.js
+++ test/observer.js
@@ -2,9 +2,11 @@ var test = require('tape');
 var mobx = require('mobx');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server')
 var TestUtils = require('react-addons-test-utils');
-var observer = require('../').observer;
-var inject = require('../').inject;
+var mobxReact = require('../');
+var observer = mobxReact.observer;
+var inject = mobxReact.inject;
 var $ = require('jquery');
 
 $("<div></div>").attr("id","testroot").appendTo($(window.document.body));
@@ -152,6 +154,71 @@ test('keep views alive', function(test) {
         }, 100);
     });
 });
+
+
+test('does not views alive when using static rendering', function(test) {
+    mobxReact.useStaticRendering(true);
+
+    var renderCount = 0;
+    var data = mobx.observable({
+        z: "hi"
+    });
+
+    var component = observer(function testComponent() {
+        renderCount++;
+        return React.createElement("div", {}, data.z);
+    });
+
+    ReactDOM.render(e(component), testRoot, function() {
+
+        test.equal(renderCount, 1);
+        test.equal($(testRoot).text(), "hi");
+
+        data.z = "hello";
+        // no re-rendering on static rendering
+
+        setTimeout(function() {
+            test.equal(renderCount, 1);
+
+            test.equal($(testRoot).text(), "hi");
+            test.equal(renderCount, 1);
+
+            test.equal(getDNode(data, "z").observers.length, 0);
+
+            mobxReact.useStaticRendering(false);
+            test.end();
+        }, 100);
+    });
+});
+
+test('does not views alive when using static + string rendering', function(test) {
+    mobxReact.useStaticRendering(true);
+
+    var renderCount = 0;
+    var data = mobx.observable({
+        z: "hi"
+    });
+
+    var component = observer(function testComponent() {
+        renderCount++;
+        return React.createElement("div", {}, data.z);
+    });
+
+    const output = ReactDOMServer.renderToStaticMarkup(e(component))
+
+    data.z = "hello";
+
+    setTimeout(function() {
+        test.equal(output, "<div>hi</div>")
+        test.equal(renderCount, 1);
+
+        test.equal(getDNode(data, "z").observers.length, 0);
+
+        mobxReact.useStaticRendering(false);
+        test.end();
+    }, 100);
+});
+
 
 // From typescript compiler:
 var __extends = (this && this.__extends) || function (d, b) {
