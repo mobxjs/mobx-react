2f8e16dd7040316197cf6fdcee739c0977ec8dc1
Michel Weststrate <mweststrate@gmail.com>
2017-06-30 14:46:31 +0200

    Added untracked file back to repo

---
+++ test/index.js
@@ -0,0 +1,16 @@
+export function createTestRoot() {
+    if (!window.document.body) { window.document.body = document.createElement("body") }
+    const testRoot = document.createElement('div');
+    document.body.appendChild(testRoot);
+    return testRoot;
+}
+
+import "./context.js"
+import "./observer.js"
+import "./issue21.js"
+import "./misc.js"
+import "./inject.js"
+import "./propTypes.js"
+import "./stateless.js"
+import "./transactions.js"
+import "./utilities.js"
