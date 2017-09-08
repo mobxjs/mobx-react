33421812d0151d49654712809127b0e08beaadfb
Peter Donald <peter@realityforge.org>
2017-09-08 22:50:15 +1000

    Run prettier over code base

--- src/index.js
+++ src/index.js
@@ -1,47 +1,45 @@
-import * as mobx from 'mobx';
-import React from 'react';
-import {unstable_batchedUpdates as rdBatched} from 'react-dom';
-import {unstable_batchedUpdates as rnBatched} from 'react-native';
+import * as mobx from "mobx"
+import React from "react"
+import { unstable_batchedUpdates as rdBatched } from "react-dom"
+import { unstable_batchedUpdates as rnBatched } from "react-native"
 
-let TARGET_LIB_NAME;
-if (__TARGET__ === 'browser') TARGET_LIB_NAME = 'mobx-react';
-if (__TARGET__ === 'native') TARGET_LIB_NAME = 'mobx-react/native';
-if (__TARGET__ === 'custom') TARGET_LIB_NAME = 'mobx-react/custom';
+let TARGET_LIB_NAME
+if (__TARGET__ === "browser") TARGET_LIB_NAME = "mobx-react"
+if (__TARGET__ === "native") TARGET_LIB_NAME = "mobx-react/native"
+if (__TARGET__ === "custom") TARGET_LIB_NAME = "mobx-react/custom"
 
-if (!mobx)
-  throw new Error(TARGET_LIB_NAME + ' requires the MobX package');
-if (!React)
-  throw new Error(TARGET_LIB_NAME + ' requires React to be available');
+if (!mobx) throw new Error(TARGET_LIB_NAME + " requires the MobX package")
+if (!React) throw new Error(TARGET_LIB_NAME + " requires React to be available")
 
-if (__TARGET__ === 'browser' && typeof rdBatched === "function")
-  mobx.extras.setReactionScheduler(rdBatched);
-if (__TARGET__ === 'native' && typeof rnBatched === "function")
-  mobx.extras.setReactionScheduler(rnBatched);
+if (__TARGET__ === "browser" && typeof rdBatched === "function")
+    mobx.extras.setReactionScheduler(rdBatched)
+if (__TARGET__ === "native" && typeof rnBatched === "function")
+    mobx.extras.setReactionScheduler(rnBatched)
 
 export {
-  observer,
-  Observer,
-  renderReporter,
-  componentByNodeRegistery,
-  trackComponents,
-  useStaticRendering
-} from './observer';
+    observer,
+    Observer,
+    renderReporter,
+    componentByNodeRegistery,
+    trackComponents,
+    useStaticRendering
+} from "./observer"
 
-export { default as Provider } from './Provider';
-export { default as inject } from './inject';
+export { default as Provider } from "./Provider"
+export { default as inject } from "./inject"
 
-import * as propTypes from './propTypes';
-export { propTypes };
-export { propTypes as PropTypes };
+import * as propTypes from "./propTypes"
+export { propTypes }
+export { propTypes as PropTypes }
 
-import { errorsReporter } from './observer';
-export const onError = fn => errorsReporter.on(fn);
+import { errorsReporter } from "./observer"
+export const onError = fn => errorsReporter.on(fn)
 
-export default exports;
+export default exports
 
 /* DevTool support */
-import { renderReporter, componentByNodeRegistery, trackComponents } from './observer';
-if (typeof __MOBX_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
-  const mobxReact = { renderReporter, componentByNodeRegistery, trackComponents };
-  __MOBX_DEVTOOLS_GLOBAL_HOOK__.injectMobxReact(mobxReact, mobx)
+import { renderReporter, componentByNodeRegistery, trackComponents } from "./observer"
+if (typeof __MOBX_DEVTOOLS_GLOBAL_HOOK__ === "object") {
+    const mobxReact = { renderReporter, componentByNodeRegistery, trackComponents }
+    __MOBX_DEVTOOLS_GLOBAL_HOOK__.injectMobxReact(mobxReact, mobx)
 }
--- test/misc.js
+++ test/misc.js
@@ -1,118 +1,134 @@
-import React, { createClass, createElement } from 'react'
-import ReactDOM from 'react-dom'
-import { mount, shallow } from 'enzyme'
-import test from 'tape'
-import mobx from 'mobx'
-import { observer } from '../'
-import {createTestRoot} from "./index"
+import React, { createClass, createElement } from "react"
+import ReactDOM from "react-dom"
+import { mount, shallow } from "enzyme"
+import test from "tape"
+import mobx from "mobx"
+import { observer } from "../"
+import { createTestRoot } from "./index"
 
-const testRoot = createTestRoot();
+const testRoot = createTestRoot()
 
-test('custom shouldComponentUpdate is not respected for observable changes (#50)', t => {
-  let called = 0;
-  const x = mobx.observable(3);
-  const C = observer(createClass({
-    render: () => <div>value:{ x.get() }</div>,
-    shouldComponentUpdate: () => called++
-  }));
-  const wrapper = mount(<C />);
-  t.equal(wrapper.find('div').text(), 'value:3');
-  t.equal(called, 0)
-  x.set(42);
-  t.equal(wrapper.find('div').text(), 'value:42');
-  t.equal(called, 0);
-  t.end();
-});
-
-test('custom shouldComponentUpdate is not respected for observable changes (#50) - 2', t => {
-  // TODO: shouldComponentUpdate is meaningless with observable props...., just show warning in component definition?
-  let called = 0;
-  const y = mobx.observable(5)
-  const C = observer(createClass({
-    render() {
-      return <div>value:{ this.props.y }</div>
-    },
-    shouldComponentUpdate(nextProps) {
-      called++;
-      return nextProps.y !== 42;
-    }
-  }));
-  const B = observer(createClass({
-    render: () =>
-      <span>
-        <C y={ y.get() } />
-      </span>
-  }));
-  const wrapper = mount(<B />);
-  t.equal(wrapper.find('div').text(), 'value:5');
-  t.equal(called, 0)
-
-  y.set(6);
-  t.equal(wrapper.find('div').text(), 'value:6');
-  t.equal(called, 1)
-
-  y.set(42)
-  // t.equal(wrapper.find('div').text(), 'value:6'); // not updated! TODO: fix
-  t.equal(called, 2)
-
-  y.set(7)
-  t.equal(wrapper.find('div').text(), 'value:7');
-  t.equal(called, 3)
-
-  t.end();
+test("custom shouldComponentUpdate is not respected for observable changes (#50)", t => {
+    let called = 0
+    const x = mobx.observable(3)
+    const C = observer(
+        createClass({
+            render: () => <div>value:{x.get()}</div>,
+            shouldComponentUpdate: () => called++
+        })
+    )
+    const wrapper = mount(<C />)
+    t.equal(wrapper.find("div").text(), "value:3")
+    t.equal(called, 0)
+    x.set(42)
+    t.equal(wrapper.find("div").text(), "value:42")
+    t.equal(called, 0)
+    t.end()
 })
 
-test('issue mobx 405', t => {
-  function ExampleState() {
-    mobx.extendObservable(this, {
-      name: 'test',
-      get greetings() {
-        return 'Hello my name is ' + this.name;
-      }
-    })
-  }
+test("custom shouldComponentUpdate is not respected for observable changes (#50) - 2", t => {
+    // TODO: shouldComponentUpdate is meaningless with observable props...., just show warning in component definition?
+    let called = 0
+    const y = mobx.observable(5)
+    const C = observer(
+        createClass({
+            render() {
+                return <div>value:{this.props.y}</div>
+            },
+            shouldComponentUpdate(nextProps) {
+                called++
+                return nextProps.y !== 42
+            }
+        })
+    )
+    const B = observer(
+        createClass({
+            render: () => (
+                <span>
+                    <C y={y.get()} />
+                </span>
+            )
+        })
+    )
+    const wrapper = mount(<B />)
+    t.equal(wrapper.find("div").text(), "value:5")
+    t.equal(called, 0)
 
-  const ExampleView = observer(createClass({
-    render() {
-      return (
-        <div>
-          <input
-            type='text'
-            onChange={ e => this.props.exampleState.name = e.target.value }
-            value={ this.props.exampleState.name } />
-          <span>{ this.props.exampleState.greetings }</span>
-        </div>
-      )
+    y.set(6)
+    t.equal(wrapper.find("div").text(), "value:6")
+    t.equal(called, 1)
+
+    y.set(42)
+    // t.equal(wrapper.find('div').text(), 'value:6'); // not updated! TODO: fix
+    t.equal(called, 2)
+
+    y.set(7)
+    t.equal(wrapper.find("div").text(), "value:7")
+    t.equal(called, 3)
+
+    t.end()
+})
+
+test("issue mobx 405", t => {
+    function ExampleState() {
+        mobx.extendObservable(this, {
+            name: "test",
+            get greetings() {
+                return "Hello my name is " + this.name
+            }
+        })
     }
-  }));
 
-  const exampleState = new ExampleState();
-  const wrapper = shallow(<ExampleView exampleState={ exampleState } />);
-  t.equal(wrapper.find('span').text(), 'Hello my name is test');
+    const ExampleView = observer(
+        createClass({
+            render() {
+                return (
+                    <div>
+                        <input
+                            type="text"
+                            onChange={e => (this.props.exampleState.name = e.target.value)}
+                            value={this.props.exampleState.name}
+                        />
+                        <span>{this.props.exampleState.greetings}</span>
+                    </div>
+                )
+            }
+        })
+    )
 
-  t.end();
-});
+    const exampleState = new ExampleState()
+    const wrapper = shallow(<ExampleView exampleState={exampleState} />)
+    t.equal(wrapper.find("span").text(), "Hello my name is test")
 
-test('#85 Should handle state changing in constructors', function(t) {
-  const a = mobx.observable(2);
-  const Child = observer(createClass({
-    displayName: 'Child',
-    getInitialState() {
-      a.set(3); // one shouldn't do this!
-      return {};
-    },
-    render: () => <div>child:{ a.get() } - </div>
-  }));
-  const ParentWrapper = observer(function Parent() {
-    return <span><Child />parent:{ a.get() }</span>
-  });
-  ReactDOM.render(<ParentWrapper />, testRoot, () => {
-    t.equal(testRoot.getElementsByTagName('span')[0].textContent, 'child:3 - parent:3');
-    a.set(5);
-    t.equal(testRoot.getElementsByTagName('span')[0].textContent, 'child:5 - parent:5');
-    a.set(7);
-    t.equal(testRoot.getElementsByTagName('span')[0].textContent, 'child:7 - parent:7');
-    testRoot.parentNode.removeChild(testRoot);
-    t.end();
-  });
-});
+    t.end()
+})
+
+test("#85 Should handle state changing in constructors", function(t) {
+    const a = mobx.observable(2)
+    const Child = observer(
+        createClass({
+            displayName: "Child",
+            getInitialState() {
+                a.set(3) // one shouldn't do this!
+                return {}
+            },
+            render: () => <div>child:{a.get()} - </div>
+        })
+    )
+    const ParentWrapper = observer(function Parent() {
+        return (
+            <span>
+                <Child />parent:{a.get()}
+            </span>
+        )
+    })
+    ReactDOM.render(<ParentWrapper />, testRoot, () => {
+        t.equal(testRoot.getElementsByTagName("span")[0].textContent, "child:3 - parent:3")
+        a.set(5)
+        t.equal(testRoot.getElementsByTagName("span")[0].textContent, "child:5 - parent:5")
+        a.set(7)
+        t.equal(testRoot.getElementsByTagName("span")[0].textContent, "child:7 - parent:7")
+        testRoot.parentNode.removeChild(testRoot)
+        t.end()
+    })
+})
