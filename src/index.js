5a033638cf824ca3d04bea65cc86178a6fe20d69
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2017-09-18 19:00:01 +0200

    Do not import start from mobx - this breaks tree-shaking

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as mobx from 'mobx';
+import {extras, spy} from 'mobx';
 import React from 'react';
 import {unstable_batchedUpdates as rdBatched} from 'react-dom';
 import {unstable_batchedUpdates as rnBatched} from 'react-native';
@@ -8,15 +8,13 @@ if (__TARGET__ === 'browser') TARGET_LIB_NAME = 'mobx-react';
 if (__TARGET__ === 'native') TARGET_LIB_NAME = 'mobx-react/native';
 if (__TARGET__ === 'custom') TARGET_LIB_NAME = 'mobx-react/custom';
 
-if (!mobx)
-  throw new Error(TARGET_LIB_NAME + ' requires the MobX package');
 if (!React)
   throw new Error(TARGET_LIB_NAME + ' requires React to be available');
 
 if (__TARGET__ === 'browser' && typeof rdBatched === "function")
-  mobx.extras.setReactionScheduler(rdBatched);
+  extras.setReactionScheduler(rdBatched);
 if (__TARGET__ === 'native' && typeof rnBatched === "function")
-  mobx.extras.setReactionScheduler(rnBatched);
+  extras.setReactionScheduler(rnBatched);
 
 export {
   observer,
@@ -42,6 +40,7 @@ export default exports;
 /* DevTool support */
 import { renderReporter, componentByNodeRegistery, trackComponents } from './observer';
 if (typeof __MOBX_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
+  const mobx = { spy, extras };
   const mobxReact = { renderReporter, componentByNodeRegistery, trackComponents };
-  __MOBX_DEVTOOLS_GLOBAL_HOOK__.injectMobxReact(mobxReact, mobx)
+  __MOBX_DEVTOOLS_GLOBAL_HOOK__.injectMobxReact(mobxReact, mobx);
 }
