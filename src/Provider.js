7e32fc0b3b10bd166d4201f4a45e1b0c4d4daaf2
Michel Weststrate <mweststrate@gmail.com>
2019-02-11 12:52:51 +0100

    Provider can now redefine / combine stores

--- src/Provider.js
+++ src/Provider.js
@@ -6,8 +6,17 @@ const specialReactKeys = { children: true, key: true, ref: true }
 export const MobXProviderContext = createContext({})
 
 class Provider extends Component {
+    static contextType = MobXProviderContext
+
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            ...context,
+            ...grabStores(props)
+        }
+    }
+
     render() {
-        console.log("Provide " + Object.keys(this.state))
         return createElement(
             MobXProviderContext.Provider,
             { value: this.state },
@@ -15,25 +24,13 @@ class Provider extends Component {
         )
     }
 
-    // getChildContext() {
-    //     const stores = {}
-    //     // inherit stores
-    //     copyStores(this.context.mobxStores, stores)
-    //     // add own stores
-    //     copyStores(this.props, stores)
-    //     return {
-    //         mobxStores: stores
-    //     }
-    // }
-
     static getDerivedStateFromProps(nextProps, prevState) {
-        const newStores = grabStores(nextProps)
-        if (!prevState) return newStores
+        const newStores = { ...prevState, ...grabStores(nextProps) } // spread in prevState for the context based stores
         if (!shallowEqual(prevState, newStores))
             throw new Error(
                 "MobX Provider: The set of provided stores has changed. Please avoid changing stores as the change might not propagate to all children"
             )
-        return newStores
+        return prevState // because they didn't change, remember!
     }
 }
 
