3e7bf8f21eacc9678e452715445864962ca2ca4d
Veniamin Krol <153412+vkrol@users.noreply.github.com>
2019-08-02 23:50:35 +0300

    Restructure Provider component tests (#746)
    * Move Provider tests from inject.test.js to Provider.test.js
    
    * Simplify Provider tests that was moved from inject.test.js
    
    * Do not use inject in Provider tests (we test Provider only, not Provider + inject)
    
    * Add tests for the Provider component

--- test/Provider.test.js
+++ test/Provider.test.js
@@ -4,6 +4,19 @@ import { render } from "@testing-library/react"
 import { MobXProviderContext } from "../src/Provider"
 
 describe("Provider", () => {
+    it("should work in a simple case", () => {
+        function A() {
+            return (
+                <Provider foo="bar">
+                    <MobXProviderContext.Consumer>{({ foo }) => foo}</MobXProviderContext.Consumer>
+                </Provider>
+            )
+        }
+
+        const { container } = render(<A />)
+        expect(container).toHaveTextContent("bar")
+    })
+
     it("should not provide the children prop", () => {
         function A() {
             return (
@@ -21,5 +34,56 @@ describe("Provider", () => {
 
         const { container } = render(<A />)
         expect(container).toHaveTextContent("children was not provided")
+    })
+
+    it("supports overriding stores", () => {
+        function B() {
+            return (
+                <MobXProviderContext.Consumer>
+                    {({ overridable, nonOverridable }) => `${overridable} ${nonOverridable}`}
+                </MobXProviderContext.Consumer>
+            )
+        }
+
+        function A() {
+            return (
+                <Provider overridable="original" nonOverridable="original">
+                    <B />
+                    <Provider overridable="overriden">
+                        <B />
+                    </Provider>
+                </Provider>
+            )
+        }
+        const { container } = render(<A />)
+        expect(container).toMatchInlineSnapshot(`
+<div>
+  original original
+  overriden original
+</div>
+`)
+    })
+
+    it("should throw an error when changing stores", () => {
+        const baseError = console.error
+        console.error = () => {}
+
+        function A({ foo }) {
+            return (
+                <Provider foo={foo}>
+                    <MobXProviderContext.Consumer>{({ foo }) => foo}</MobXProviderContext.Consumer>
+                </Provider>
+            )
+        }
+
+        const { rerender } = render(<A foo={1} />)
+
+        expect(() => {
+            rerender(<A foo={2} />)
+        }).toThrow(
+            "The set of provided stores has changed. Please avoid changing stores as the change might not propagate to all children"
+        )
+
+        console.error = baseError
     })
 })
