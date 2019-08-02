9dc599e75ea1a89083e37122f2d9f2aeed97e06c
Veniamin Krol <153412+vkrol@users.noreply.github.com>
2019-08-02 22:21:48 +0300

    Simplify Provider tests that was moved from inject.test.js

--- test/Provider.test.js
+++ test/Provider.test.js
@@ -1,8 +1,6 @@
-import React, { Component } from "react"
-import * as mobx from "mobx"
-import { observer, inject, Provider } from "../src"
+import React from "react"
+import { inject, Provider } from "../src"
 import { render } from "@testing-library/react"
-import renderer, { act } from "react-test-renderer"
 import { MobXProviderContext } from "../src/Provider"
 
 describe("Provider", () => {
@@ -26,123 +24,54 @@ describe("Provider", () => {
     })
 
     it("supports overriding stores", () => {
-        const C = inject("foo", "bar")(
-            observer(
-                class C extends Component {
-                    render() {
-                        return (
-                            <div>
-                                context:
-                                {this.props.foo}
-                                {this.props.bar}
-                            </div>
-                        )
-                    }
-                }
-            )
-        )
-        const B = () => <C />
-        const A = class A extends Component {
-            render() {
-                return (
-                    <Provider foo="bar" bar={1337}>
-                        <div>
-                            <span>
-                                <B />
-                            </span>
-                            <section>
-                                <Provider foo={42}>
-                                    <B />
-                                </Provider>
-                            </section>
-                        </div>
+        const B = inject("overridable", "nonOverridable")(function({
+            overridable,
+            nonOverridable
+        }) {
+            return `${overridable} ${nonOverridable}`
+        })
+
+        function A() {
+            return (
+                <Provider overridable="original" nonOverridable="original">
+                    <B />
+                    <Provider overridable="overriden">
+                        <B />
                     </Provider>
-                )
-            }
+                </Provider>
+            )
         }
-        const wrapper = renderer.create(<A />)
-        expect(wrapper).toMatchInlineSnapshot(`
+        const { container } = render(<A />)
+        expect(container).toMatchInlineSnapshot(`
 <div>
-  <span>
-    <div>
-      context:
-      bar
-      1337
-    </div>
-  </span>
-  <section>
-    <div>
-      context:
-      42
-      1337
-    </div>
-  </section>
+  original original
+  overriden original
 </div>
 `)
     })
 
     it("should throw an error when changing stores", () => {
-        let msgs = []
         const baseError = console.error
-        console.error = m => msgs.push(m.split("\n").slice(0, 7)) // drop stacktraces to avoid line number issues
-        const a = mobx.observable.box(3)
-        const C = inject("foo")(
-            observer(
-                class C extends Component {
-                    render() {
-                        return (
-                            <div>
-                                context:
-                                {this.props.foo}
-                            </div>
-                        )
-                    }
-                }
-            )
-        )
-        const B = observer(
-            class B extends Component {
-                render() {
-                    return <C />
-                }
-            }
-        )
-        const A = observer(
-            class A extends Component {
-                render() {
-                    return (
-                        <section>
-                            <span>{a.get()}</span>
-                            <Provider foo={a.get()}>
-                                <B />
-                            </Provider>
-                        </section>
-                    )
-                }
-            }
-        )
-        const wrapper = renderer.create(<A />)
+        console.error = () => {}
+        const B = inject("foo")(function C({ foo }) {
+            return foo
+        })
 
-        expect(wrapper).toMatchInlineSnapshot(`
-<section>
-  <span>
-    3
-  </span>
-  <div>
-    context:
-    3
-  </div>
-</section>
-`)
+        function A({ foo }) {
+            return (
+                <Provider foo={foo}>
+                    <B />
+                </Provider>
+            )
+        }
+
+        const { rerender } = render(<A foo={1} />)
 
         expect(() => {
-            act(() => {
-                a.set(42)
-            })
+            rerender(<A foo={2} />)
         }).toThrow(
             "The set of provided stores has changed. Please avoid changing stores as the change might not propagate to all children"
         )
-        expect(msgs).toMatchSnapshot() // nobody caught the error of the rendering
 
         console.error = baseError
     })
