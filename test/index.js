47992887ff980016cad89dbee06e8cccde52b87e
Veniamin Krol <153412+vkrol@users.noreply.github.com>
2019-08-04 10:46:57 +0300

    Setup missing React act(...) warning workaround globally in tests

--- test/index.js
+++ test/index.js
@@ -1,12 +1,3 @@
-// Uglyness to find missing 'act' more easily
-// 14-2-19 / React 16.8.1, temporarily work around, as error message misses a stack-trace
-Error.stackTraceLimit = Infinity
-const origError = console.error
-console.error = function(msg) {
-    if (/react-wrap-tests-with-act/.test("" + msg)) throw new Error("missing act")
-    return origError.apply(this, arguments)
-}
-
 export function withConsole(fn) {
     const { warn, error, info } = global.console
     const warnings = []
