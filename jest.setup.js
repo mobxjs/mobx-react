8cc2b39dab7d774d127f7803cac859a58a07d382
Veniamin Krol <153412+vkrol@users.noreply.github.com>
2019-08-04 12:19:12 +0300

    Setup missing React act(...) warning workaround globally in tests (#749)

---
+++ jest.setup.js
@@ -0,0 +1,8 @@
+// Uglyness to find missing 'act' more easily
+// 14-2-19 / React 16.8.1, temporarily work around, as error message misses a stack-trace
+Error.stackTraceLimit = Infinity
+const origError = console.error
+console.error = function(msg) {
+    if (/react-wrap-tests-with-act/.test("" + msg)) throw new Error("missing act")
+    return origError.apply(this, arguments)
+}
