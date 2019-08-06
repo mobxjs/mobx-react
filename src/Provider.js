1e8d42e37d8d6d5ea103c17022cb25865501fc2c
Veniamin Krol <153412+vkrol@users.noreply.github.com>
2019-08-07 00:03:11 +0300

    Simplify the Provider component by using object destructuring (thanks to @urugator)
    https://github.com/mobxjs/mobx-react/issues/745#issuecomment-518415498

--- src/Provider.js
+++ src/Provider.js
@@ -4,15 +4,15 @@ import { shallowEqual } from "./utils/utils"
 
 export const MobXProviderContext = React.createContext({})
 
-export function Provider(props) {
+export function Provider({ children, ...stores }) {
     const parentValue = React.useContext(MobXProviderContext)
     const value = React.useRef({
         ...parentValue,
-        ...grabStores(props)
+        ...stores
     }).current
 
     if (process.env.NODE_ENV !== "production") {
-        const newValue = { ...value, ...grabStores(props) } // spread in previous state for the context based stores
+        const newValue = { ...value, ...stores } // spread in previous state for the context based stores
         if (!shallowEqual(value, newValue)) {
             throw new Error(
                 "MobX Provider: The set of provided stores has changed. Please avoid changing stores as the change might not propagate to all children"
@@ -20,16 +20,7 @@ export function Provider(props) {
         }
     }
 
-    return (
-        <MobXProviderContext.Provider value={value}>{props.children}</MobXProviderContext.Provider>
-    )
+    return <MobXProviderContext.Provider value={value}>{children}</MobXProviderContext.Provider>
 }
 
 Provider.displayName = "MobXProvider"
-
-function grabStores(from) {
-    const res = {}
-    if (!from) return res
-    for (let key in from) if (key !== "children") res[key] = from[key]
-    return res
-}
